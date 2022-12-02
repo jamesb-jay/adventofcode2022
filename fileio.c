c299a91486afa996d4371ec5098e3c2bcec94f40
James Bennett <psyjb23@nottingham.ac.uk>
2022-12-02 09:55:24 +0000

    Building fileio lib

--- fileio.c
+++ fileio.c
@@ -1,7 +1,7 @@
 #include "headers/fileio.h"
 #include <stdio.h>
 
-int countFileLines(FILE *f)
+int countFileLines(FILE *file)
 {
 	char ch = '0';
 	int lines = 0;
@@ -12,4 +12,37 @@ int countFileLines(FILE *f)
 
 	rewind(file);
 	return lines;
+}
+
+char **loadFileStrings(FILE *file, int maxLineSize)
+{
+	char lineBuffer = calloc(maxLineSize, sizeof(char));
+    char **fileStrings;
+
+	int lines = 0, sum = 0, i = 0;
+
+    /* Get number of lines in file */
+    lines = countFileLines(file);
+    if (!lines)
+        return NULL;
+
+    /* Allocate memory to load strings */
+    fileStrings = calloc(lines, sizeof(char*));
+
+    for (i = 0; i < lines; i++)
+        fileStrings = calloc(maxLineSize, );
+
+
+	while (fgets(lineBuffer, maxLineSize, file) != NULL)
+	{
+		
+	}
+
+	rewind(file);
+	return;
+}
+
+void closeFileStrings(FILE *f)
+{
+
 }
--- headers/fileio.h
+++ headers/fileio.h
@@ -2,6 +2,12 @@
 
 #define _FILEIO_H
 
+/* Count the number of lines in a file */
 int countFileLines(FILE *f);
+
+/* Load an array of strings containing each line of the file. */
+char **loadFileStrings(FILE *f, int maxLineSize);
+
+void closeFileStrings(FILE *f);
 
 #endif
