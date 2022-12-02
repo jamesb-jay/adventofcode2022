a7318b19a86d61135649584ad5181086b1f0f4ea
James Bennett <psyjb23@nottingham.ac.uk>
2022-12-02 21:51:12 +0000

    1 finished

--- day2.c
+++ day2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "headers/fileio.h"
 
+int getGameResult(char player, char opponent);
+int calculateScore(LoadedFile *input);
+int getPoints(char choice);
 
 int main(int argc, char **argv)
 {
 	FILE *inputFile;
     LoadedFile input;
-    int i;
 
 	/* Check enough args */
 	if (argc < 2)
@@ -25,6 +28,7 @@ int main(int argc, char **argv)
     inputFile = fopen(argv[1], "r");
     input = loadFile(inputFile, 16);
 
+    printf("Score: %d\n", calculateScore(&input));
 
     closeFile(input);
     fclose(inputFile);
@@ -36,7 +40,7 @@ int getGameResult(char player, char opponent)
 {
     /*
     ascii codes:
-    opponent - player:
+    player - opponent:
             X   Y   Z
         A  23  24  25
 
@@ -47,13 +51,59 @@ int getGameResult(char player, char opponent)
     =23 draw
     <23 loss
     */
-    
-    int result = (int)(opponent - player);
 
-    if (result > 23)
-        return 1;
-    else if (result < 23)
+    
+    int result = (int)(player - opponent);
+    if (result == 23)
+        return 0;
+    else if (result == 25 || result == 22)
         return -1;
     else
-        return 0;
+        return 1;
+}
+
+int calculateScore(LoadedFile *input)
+{
+    int i, result, score = 0;
+    char *buf, me, opp;
+
+    for (i = 0; i < input->lineCount; i++)
+    {
+        buf = strtok(input->lineArray[i], " ");
+        opp = buf[0];
+        buf = strtok(NULL, " ");
+        me = buf[0];
+
+        result = getGameResult(me, opp);
+        if (result > 0)
+        {
+            /* Win! */
+            score += getPoints(me) + 6;
+
+        }
+        else if (result < 0)
+        {
+            /* Lose */
+            score += getPoints(me);
+        }
+        else
+        {
+            /* Draw */
+            score += getPoints(me) + 3;
+        }
+    }
+
+    return score;
+}
+
+int getPoints(char choice)
+{
+    if (choice == 'A' || choice == 'X')
+        return 1;
+    if (choice == 'B' || choice == 'Y')
+        return 2;
+    if (choice == 'C' || choice == 'Z')
+        return 3;
+    
+    return 0;
 }
